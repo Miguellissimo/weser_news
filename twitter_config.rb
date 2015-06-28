adcc1cb2f1ba351453c8cb2685605256c46baa72
Miguel <miguellissimo@gmail.com>
2015-06-28 23:27:33 +0200

    twitter config

--- twitter_config.rb
+++
@@ -1,9 +0,0 @@
-require 'twitter'
-
-$client = Twitter::REST::Client.new do |config|
-  config.consumer_key = "xxxx-xxxx-xxxx-xxxx"
-  config.consumer_secret = "xxxx-xxxx-xxxx-xxxx"
-  config.access_token = "xxxx-xxxx-xxxx-xxxx"
-  config.access_token_secret = "xxxx-xxxx-xxxx-xxxx"
-end
-
